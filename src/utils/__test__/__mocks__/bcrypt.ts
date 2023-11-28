5a33efe4c3208d68fab2f13e7925714bcf61c2b9
Olanipekun Adekunle Oluwole <56478873+kunle001@users.noreply.github.com>
2023-11-28 13:27:28 +0100

    Delete src/utils/__test__ directory

--- src/utils/__test__/__mocks__/bcrypt.ts
+++
@@ -1,6 +0,0 @@
-// __mocks__/bcrypt.js
-
-export const hash = jest.fn().mockResolvedValue('mockedHashedPassword');
-export const compare = jest.fn().mockImplementation((suppliedPassword, hashedPassword) => {
-  return suppliedPassword === 'password123' && hashedPassword === 'mockedHashedPassword';
-});
